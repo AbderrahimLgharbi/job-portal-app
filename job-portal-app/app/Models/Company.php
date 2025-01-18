6ec7fa4eadc7b6912c017c32282dfd0b0f0f9b2c
Abderrahim LGHARBI <abderrahimdev@gmail.com>
2025-01-18 01:14:58 +0100

    add sluggable config with our company name

--- job-portal-app/app/Models/Company.php
+++ job-portal-app/app/Models/Company.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Company extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     protected $fillable = [
         'user_id',
@@ -18,4 +20,13 @@ class Company extends Model
         'bio',
         'vision'
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
